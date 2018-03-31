44f7db581256554a1ca08ab7f2a04098ee1c5956
Terashima <yamanogomora@gmail.com>
2018-03-31 16:49:00 +0900

    告知のための仮デザイン

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge7">
+	<meta name="viewport" content="width=device-width initial-scale=1">
+	<title>空家レンジャー</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+</head>
+<body>
+<header>
+  <div class="container clearfix">
+    <div class="logo">
+      <a href="<?php echo home_url() ?>">
+        <img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/logo.png" width="69" height="69" alt="空家レンジャー">
+      </a>
+    </div>
+    <div class="message">詳細情報や予約受付は準備中です！</div>
+  </div>
+</header>
--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<html lang="ja">
-<head>
-<title></title>
-</head>
-<body>
-</body>
-</html>
+<?php get_header(); ?>
+<div>
+	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_1.jpg" width="1000" height="1415" class="responsive-img">
+</div>
+<div>
+	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_2.jpg" width="1000" height="1415" class="responsive-img">
+</div>
+<div>
+	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_3.jpg" width="1000" height="1479" class="responsive-img">
+</div>
+<div>
+	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_4.jpg" width="1000" height="1400" class="responsive-img">
+</div>
+<div>
+	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_5.jpg" width="1000" height="57" class="responsive-img">
+</div>
+<?php get_footer(); ?>
