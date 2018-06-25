b71c418ecbff35dd391a7718d317f4d81d88622a
Terashima <yamanogomora@gmail.com>
2018-06-25 16:19:34 +0900

    ハンバーガーメニューを追加

--- header.php
+++ header.php
@@ -1,20 +1,49 @@
 <!DOCTYPE html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge7">
-	<meta name="viewport" content="width=device-width initial-scale=1">
-	<title>空家レンジャー</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge7">
+  <meta name="viewport" content="width=device-width initial-scale=1">
+  <title>空家レンジャー</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
 </head>
 <body>
 <header>
-  <div class="container clearfix">
+  <div class="container">
+    <div class="menu">
+      <div id="nav-drawer">
+        <input id="nav-input" type="checkbox" class="nav-unshown">
+        <label id="nav-open" for="nav-input"><span></span></label>
+        <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+        <div id="nav-content">
+          <div class="head">
+            <label class="nav-unshown" for="nav-input">MENU</label>
+          </div>
+          <nav>
+            <a href="">
+              <div class="jp">空家レンジャー</div>
+              <hr>
+              <div class="eng">AKIYA RANGER</div>
+            </a>
+            <a href="">
+              <div class="jp">葉山ファクトリー</div>
+              <hr>
+              <div class="eng">HAYAMA FACTORY</div>
+            </a>
+            <a href="">
+              <div class="jp">葉山キッチン</div>
+              <hr>
+              <div class="eng">HAYAMA KITCHEN</div>
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
     <div class="logo">
       <a href="<?php echo home_url() ?>">
-        <img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/logo.png" width="69" height="69" alt="空家レンジャー">
+        <img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/logo.png" width="50" height="50" alt="空家レンジャー">
       </a>
     </div>
-    <div class="message">詳細情報や予約受付は準備中です！</div>
+    <div class="login"></div>
   </div>
 </header>
--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php get_header(); ?>
-<div>
-	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_1.jpg" width="1000" height="1415" class="responsive-img">
-</div>
-<div>
-	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_2.jpg" width="1000" height="1415" class="responsive-img">
-</div>
-<div>
-	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_3.jpg" width="1000" height="1479" class="responsive-img">
-</div>
-<div>
-	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_4.jpg" width="1000" height="1400" class="responsive-img">
-</div>
-<div>
-	<img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_5.jpg" width="1000" height="57" class="responsive-img">
-</div>
+<section class="main-content">
+  <div class="container">
+    <div><img src="http://nogelab.sakura.ne.jp/akiyaranger/wp-content/uploads/2018/03/image_1.jpg" width="1000" height="1415" class="responsive-img"></div>
+  </div>
+</section>
 <?php get_footer(); ?>
