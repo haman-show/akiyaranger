2179d355ad09b73e4b7026e21ac6e9ee93fa25f0
Terashima <yamanogomora@gmail.com>
2018-06-26 16:57:01 +0900

    facebook page プラグインをfooter.php に移動

--- footer.php
+++ footer.php
@@ -1,2 +1,17 @@
+<footer>
+  <div class="container">
+    <div class="fb-plugin-holder clearfix">
+      <div class="fb-plugin-cell">
+        <div class="fb-page" data-href="https://www.facebook.com/akiyaranger/" data-tabs="timeline,events" data-width="310" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/akiyaranger/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/akiyaranger/">空家レンジャー</a></blockquote></div>
+      </div>
+      <div class="fb-plugin-cell">
+        <div class="fb-page" data-href="https://www.facebook.com/hayamafactory/" data-tabs="timeline,events" data-width="310" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/hayamafactory/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/hayamafactory/">葉山ファクトリー</a></blockquote></div>
+      </div>
+      <div class="fb-plugin-cell">
+        <div class="fb-page" data-href="https://www.facebook.com/hayamakitchen/" data-tabs="timeline,events" data-width="310" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/hayamakitchen/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/hayamakitchen/">葉山キッチン Hayama Kitchen</a></blockquote></div>
+      </div>
+    </div>
+  </div>
+</footer>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -1,17 +1,7 @@
 <?php get_header(); ?>
 <section class="main-content">
   <div class="container">
-    <div class="fb-plugin-holder clearfix">
-      <div class="fb-plugin-cell">
-        <div class="fb-page" data-href="https://www.facebook.com/akiyaranger/" data-tabs="timeline,events" data-width="310" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/akiyaranger/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/akiyaranger/">空家レンジャー</a></blockquote></div>
-      </div>
-      <div class="fb-plugin-cell">
-        <div class="fb-page" data-href="https://www.facebook.com/hayamafactory/" data-tabs="timeline,events" data-width="310" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/hayamafactory/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/hayamafactory/">葉山ファクトリー</a></blockquote></div>
-      </div>
-      <div class="fb-plugin-cell">
-        <div class="fb-page" data-href="https://www.facebook.com/hayamakitchen/" data-tabs="timeline,events" data-width="310" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/hayamakitchen/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/hayamakitchen/">葉山キッチン Hayama Kitchen</a></blockquote></div>
-      </div>
-    </div>
+  
   </div>
 </section>
 <?php get_footer(); ?>
