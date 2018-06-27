45ccbecf0a136a3046b055bbd18229c22885949b
Terashima <yamanogomora@gmail.com>
2018-06-27 12:36:15 +0900

    お問い合わせフォームを追加

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
 <footer>
   <div class="container">
+    <div class="comp-page-title">
+      <h1>お問い合わせ</h1>
+      <hr>
+      <h2>CONTACT</h2>
+    </div>
+    <?php echo do_shortcode('[contact-form-7 id="15" title="コンタクトフォーム 1"]'); ?>
+  </div>
+  <div class="container">
     <div class="fb-plugin-holder clearfix">
       <div class="fb-plugin-cell">
         <div class="fb-page" data-href="https://www.facebook.com/akiyaranger/" data-tabs="timeline,events" data-width="310" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/akiyaranger/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/akiyaranger/">空家レンジャー</a></blockquote></div>
--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php get_header(); ?>
+<?php if(have_posts()) {while(have_posts()){the_post(); ?>
+<?php
+    $slug = $post->post_name;
+    $slug = strtoupper($slug);
+?>
 <section class="main-content">
   <div class="container">
-    <?php if(have_posts()) {while(have_posts()){the_post(); ?>
-      <?php the_content(); ?>
-    <?php }} ?>
+    <div class="comp-page-title">
+      <h1><?php the_title(); ?></h1>
+      <hr>
+      <h2><?php echo($slug); ?></h2>
+    </div>
+    <?php the_content(); ?>
   </div>
 </section>
+<?php }} ?>
 <?php get_footer(); ?>
