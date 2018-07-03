0e61437860c703f3a2d8a3911e6e1e02fe4e517e
Terashima <yamanogomora@gmail.com>
2018-07-03 14:09:06 +0900

    特設コンテンツ index テキストを追加

--- page-hhf.php
+++ page-hhf.php
@@ -16,18 +16,138 @@ Template Name: Humans of HAYAMA FACTORY のテンプレート
       </h1>
     </div>
     <div class="list clearfix">
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_1.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_2.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_3.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_4.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_5.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_6.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_1.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_2.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_3.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_4.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_5.jpg" class="responsive-img"></div>
-      <div class="box"><img src="<?php echo get_template_directory_uri(); ?>/images/human_6.jpg" class="responsive-img"></div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_1.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>コーヒーレンジャー</li>
+              <li>ミスター葉山ファクトリー</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_2.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>アースレンジャー</li>
+              <li>葉山キッチン代表</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_3.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>パンプキンレンジャー</li>
+              <li>ママアフリカ</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_4.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>チャレンジャー</li>
+              <li>廃材クリエイター</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_5.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>オーロラレンジャー</li>
+              <li>カラーセラピスト</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_6.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>ピンクレンジャー</li>
+              <li>空家レンジャー代表</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_1.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>コーヒーレンジャー</li>
+              <li>ミスター葉山ファクトリー</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_2.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>アースレンジャー</li>
+              <li>葉山キッチン代表</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_3.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>パンプキンレンジャー</li>
+              <li>ママアフリカ</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_4.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>チャレンジャー</li>
+              <li>廃材クリエイター</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_5.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>オーロラレンジャー</li>
+              <li>カラーセラピスト</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/human_6.jpg" class="responsive-img"></div>
+	<div class="text">
+          <div class="holder">
+            <ul>
+              <li>ピンクレンジャー</li>
+              <li>空家レンジャー代表</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </section>
